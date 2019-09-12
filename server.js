c51229aab0a0ca30949d84a16343c1b8d590684d
Robert Wise <robcodehub@gmail.com>
2019-09-12 06:11:03 +0200

    renders product name and price to index.html

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const path = require('path');
 const app = express();
-const db = require('./db');
+const db = require('./db')('./products.json');
+
+
+app.use((req, res, next) => {
+  next();
+});
+
 
 app.use(express.json());
 
 app.get('/', (req, res, next)=> {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.get('/api/products', async(req, res, next)=> {
+  try {
+    res.send(await db.findAll())
+  }
+  catch(ex) {
+    next(ex)
+  }
+});
+
+app.post('/api/products', async(req, res, next)=> {
+  try {
+    res.send(await db.create(req.body));
+  }
+  catch(ex) {
+    next(ex)
+  }
 });
 
 
